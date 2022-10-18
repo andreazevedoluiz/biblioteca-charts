e215e1c7c2203032aff7217af97f0af213965efc
André luiz azevedo <andreazevedoluiz@gmail.com>
2022-10-18 16:58:57 -0300

    first commit

---
+++ assets/php/conexao.php
@@ -0,0 +1,10 @@
+<?php
+$servermane = "localhost";
+$database = "graficos";
+$username = "root"
+$password = "";
+$conexao = mysqli_connect($servermane, $username, $password, $database);
+
+
+
+?>
---
+++ assets/php/teste.php
@@ -0,0 +1,42 @@
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Mês", "Lucro-bruto"],
+        <?php
+        include 'conexao.php';
+        $sql = "SELECT * FROM grafico";
+        $buscar = mysqli_query($conexao, $sql);
+
+        while ($dados = mysqli_fetch_array($buscar)){
+          $mes = $dados['Mês'];
+          $lucro = $dados['lucro-bruto'];
+        
+        ?>
+        ['<?php echo $mes ?>', '<?php echo $lucro ?>', #000000],
+        
+        <?php } ?>
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Density of Precious Metals, in g/cm^3",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+      chart.draw(view, options);
+  }
+  </script>
+<div id="columnchart_values" style="width: 900px; height: 300px;"></div>
